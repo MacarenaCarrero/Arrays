407c740ae4541598bec43e2d51ff173764bc1126
MacarenaCarrero <macarena.c.rios@gmail.com>
2024-10-24 00:12:20 +0200

    Arrays

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+//primero
+
+const randomNumber = (array) =>{
+const randomPosition = Math.floor(Math.random() * array.length);
+
+console.log (array[randomPosition]);
+
+}
+
+randomNumber([2,3,4,6,8])
+
+//segundo
+
+const numbers =(array) => {
+    console.log (numbers)
+    return array[0] +array [1]+array[2]+array[3]===numbers
+
+}
+
+numbers(2,8,6)
